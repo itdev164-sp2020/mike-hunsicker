1588c2e0388434ae4f609f84e1cea0b35cc6ad56
Mike Hunsicker <hunsickm@matc.edu>
2020-03-02 10:46:20 -0600

    Updated blog title; completed Activity 5

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -4,7 +4,7 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Blog`,
+    title: `Cool Blog`,
     description: `ITDEV-164 Gatsby Blog`,
     author: `Mike Hunsicker`,
     contact: {
